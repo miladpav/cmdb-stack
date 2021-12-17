9a7b83d7c16fdf01b940a59af16c0ceaf417e722
MilaD Allahverdi <milad.alhvrd@gmail.com>
2021-12-17 15:18:51 +0330

    improvement in commands.sh

--- commands.sh
+++ commands.sh
@@ -8,23 +8,25 @@
 ## docker-compose build
 ## docker-compose up -d
 
-## mkdir ssh-keys
 ## ssh-keygen -t rsa -b 4096 -f ssh-keys/root-id_rsa -N ''
 ## ssh-copy-id -i ssh-keys/root-id_rsa root@Servers
 ## add servers ip to agent-hosts inventory file with [agent] group
+## echo "192.168.40.134" >> /inventory/agent-hosts
 
 ## insatll agent on servers
 docker exec ansible \
 ansible-playbook -i /inventory/agent-hosts \
 -e ansible_ssh_private_key_file=/ssh-keys/root-id_rsa \
--e yourDomain=192.168.40.135 \
+-e yourDomain=192.168.40.135 \ #example: docker_server=192.168.40.135 and if you have domain you must change nginx-conf/frontend.conf
 /playbooks/tmway-agent/agent_install.yml
+
+## now your auto inventory has been created !
 
 ## gather facts from servers
 docker exec ansible \
 ansible-playbook -i /inventory/hosts.ini \
 -e ansible_ssh_private_key_file=/ssh-keys/root-id_rsa \
-/plays/gather-facts/gather-facts.yml
+/playbooks/gather-facts/gather-facts.yml
 
 ## create cmdb templates
 docker exec cmdb \
