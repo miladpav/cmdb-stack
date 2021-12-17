32214248fff7f74259771210f07295a431a192b4
MilaD Allahverdi <milad.alhvrd@gmail.com>
2021-12-17 14:00:13 +0330

    improvement agent script and change hostname-template-pattern

--- playbooks/tmway-agent/files/agent.sh
+++ playbooks/tmway-agent/files/agent.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-IP=$(ip a | grep eth0 -A 100 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -1)
+IP=$(ip a | grep $(ip link | awk -F: '$0 !~ "lo|vir|tun|docker|wl|br|^[^0-9]"{print $2;getline}') -A 5 | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -1)
 HOSTNAME=$(hostname)
 URL=$1
 
